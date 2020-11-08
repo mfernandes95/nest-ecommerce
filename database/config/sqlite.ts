03f4574cf54a3411bb7b0d98b255b22d1e8deb4d
mfernandes95 <matheusblizard@gmail.com>
2020-11-08 05:01:56 -0300

    feat config userRepo on ser test

--- database/config/sqlite.ts
+++ database/config/sqlite.ts
@@ -3,11 +3,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const options: TypeOrmModuleOptions = {
   type: 'sqlite',
-  database: '../ecomerce.db',
+  database: '../ecommerce.db',
   logging: true,
   entities: [path.resolve(__dirname, '..', '..', 'src', '**', 'entity', '*.entity{.ts,.js}')],
   migrations: [path.resolve(__dirname, '..', 'migrations', '*')],
-  // synchronize: true,
+  synchronize: true,
+  // logging: true,
+  dropSchema: true,
+  keepConnectionAlive: true
 };
 
 module.exports = options;
--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -10,6 +10,7 @@ import * as SqliteConfig from '.././../database/config/sqlite';
 
 describe('UserController', () => {
   let userController: UserController;
+  let userService: UserService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -23,6 +24,7 @@ describe('UserController', () => {
       providers: [UserService],
     }).compile();
 
+    userService = module.get<UserService>(UserService);
     userController = module.get<UserController>(UserController);
   });
 
@@ -33,4 +35,20 @@ describe('UserController', () => {
     });
   })
 
+  describe('User', () => {
+      it('Should get users', async () => {
+        const result = await userService.createUser({
+          name: 'Matheus',
+          email: 'msamsa@email.com',
+          password: "wdawd"
+        })
+
+        console.log('resss', result);
+
+        // jest.spyOn(userService, 'find')
+        //   .mockImplementationOnce(async () => ( id: 'my-id' ));
+        // console.log('333333333', await userService.find());
+        expect(await userService.find()).toEqual([result])
+      });
+    })
 });
