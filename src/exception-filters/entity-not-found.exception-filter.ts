b6bee5be8a9bb4e9e98c6f0b00e1b136f89388cd
mpinheiro20 <matheus.pinheiro@elumini-it.com>
2020-12-17 14:33:31 -0300

    feat log notFoundException

--- src/exception-filters/entity-not-found.exception-filter.ts
+++ src/exception-filters/entity-not-found.exception-filter.ts
@@ -1,14 +1,27 @@
-import { ExceptionFilter, ArgumentsHost } from '@nestjs/common';
+import { ExceptionFilter, ArgumentsHost, Inject } from '@nestjs/common';
 import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError';
 import { Response } from 'express';
+import { Logger } from 'winston';
+
 
 export class EntityNotFoundExceptionFilter implements ExceptionFilter {
+
+  constructor(
+    @Inject('winston')
+    private logger: Logger
+  ) { }
+
   catch(exception: EntityNotFoundError, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    console.log('exeppp', exception);
 
-    // if (Node.env == production) {
+    // if (process.env.NODE_ENV === 'production') {
+
+    //   this.logger.info({
+    //     timestamp: new Date().toISOString(),
+    //     data: exception.message,
+    //   })
+
     //   return response
     //     .status(404)
     //     .json({
